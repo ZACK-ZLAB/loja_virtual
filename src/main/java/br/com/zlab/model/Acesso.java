1429eac4e5a6aafed92f5d82e6db7f9ec67c9efc
zack junior <zacarias1992.12.19@gmail.com>
2023-11-13 16:16:35 -0300

    created test for access salve end point

--- src/main/java/br/com/zlab/model/Acesso.java
+++ src/main/java/br/com/zlab/model/Acesso.java
@@ -4,6 +4,8 @@ import java.util.Objects;
 
 import org.springframework.security.core.GrantedAuthority;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,6 +27,7 @@ public class Acesso implements GrantedAuthority {
 	@Column(nullable = false)
 	private String descricao; /* Acesso ex: ROLE_ADMIN ou ROLE_SECRETARIO */
 
+	@JsonIgnore
 	@Override
 	public String getAuthority() {
 		return this.descricao;
--- src/test/java/br/com/zlab/LojaVirtualApplicationTests.java
+++ src/test/java/br/com/zlab/LojaVirtualApplicationTests.java
@@ -5,6 +5,15 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.zlab.controller.AcessoController;
 import br.com.zlab.model.Acesso;
@@ -12,60 +21,85 @@ import br.com.zlab.repository.AcessoRepository;
 import junit.framework.TestCase;
 
 @SpringBootTest(classes = LojaVirtualApplication.class)
-public class LojaVirtualApplicationTests extends TestCase{
-	
+public class LojaVirtualApplicationTests extends TestCase {
+
 	@Autowired
 	private AcessoController acessoController;
-	
+
 	@Autowired
 	private AcessoRepository acessoRepository;
-	
-	
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	public void testRestApiCadastroAcesso() throws JsonProcessingException, Exception {
+
+		DefaultMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.wac);
+		MockMvc mockMvc = builder.build();
+
+		Acesso acesso = new Acesso();
+
+		acesso.setDescricao("ROLE_COMPRADOR");
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		ResultActions retornoApi = mockMvc
+				.perform(MockMvcRequestBuilders.post("/salvarAcesso").content(objectMapper.writeValueAsString(acesso))
+						.accept(org.springframework.http.MediaType.APPLICATION_JSON)
+						.contentType(org.springframework.http.MediaType.APPLICATION_JSON));
+		
+		System.out.println("Retorno API" + retornoApi.andReturn().getResponse().getContentAsString());
+
+		Acesso objetoRetorno = objectMapper.readValue(retornoApi.andReturn().getResponse().getContentAsString(), Acesso.class);
+		
+		assertEquals(acesso.getDescricao(), objetoRetorno.getDescricao());
+
+	}
 
 	@Test
 	public void testCadastraAcesso() {
-		
+
 		Acesso acesso = new Acesso();
-		
+
 		acesso.setDescricao("ROLE_ADMIN");
-		
+
 		assertEquals(true, acesso.getId() == null);
-		
-		/*Gravou no banco de dados*/
+
+		/* Gravou no banco de dados */
 		acesso = acessoController.salvarAcesso(acesso).getBody();
-		
+
 		assertEquals(true, acesso.getId() > 0);
-		
-		/*Valida dados salvos da forma correta*/
+
+		/* Valida dados salvos da forma correta */
 		assertEquals("ROLE_ADMIN", acesso.getDescricao());
-		
-		/*Teste de carregamento*/
+
+		/* Teste de carregamento */
 		Acesso acesso2 = acessoRepository.findById(acesso.getId()).get();
-		
+
 		assertEquals(acesso.getId(), acesso2.getId());
-		
-		/*Teste de delete*/
-		
+
+		/* Teste de delete */
+
 		acessoRepository.deleteById(acesso2.getId());
-		
-		acessoRepository.flush();/*roda SQL de ddelete no banco*/
-		
+
+		acessoRepository.flush();/* roda SQL de ddelete no banco */
+
 		Acesso acesso3 = acessoRepository.findById(acesso2.getId()).orElse(null);
-		
+
 		assertEquals(true, acesso3 == null);
-		
-		/*Teste de query*/
-		
+
+		/* Teste de query */
+
 		acesso = new Acesso();
-		
+
 		acesso.setDescricao("ROLE_ALUNO");
-		
+
 		acesso = acessoController.salvarAcesso(acesso).getBody();
-		
+
 		List<Acesso> acessos = acessoRepository.buscarAcessoDesc("ALUNO".trim().toUpperCase());
-		
+
 		assertEquals(1, acessos.size());
-		
+
 		acessoRepository.deleteById(acesso.getId());
 	}
 
