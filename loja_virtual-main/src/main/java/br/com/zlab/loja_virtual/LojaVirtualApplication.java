cc3500729a840bd91cce3a59bced2817f9a6e5c9
zack junior <zacarias1992.12.19@gmail.com>
2024-03-06 21:19:10 -0300

    payment notification juno API

--- loja_virtual-main/src/main/java/br/com/zlab/loja_virtual/LojaVirtualApplication.java
+++ loja_virtual-main/src/main/java/br/com/zlab/loja_virtual/LojaVirtualApplication.java
@@ -11,7 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@Profile("devp")
+@Profile("output")
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
