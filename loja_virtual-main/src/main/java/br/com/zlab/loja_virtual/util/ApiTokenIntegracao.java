fcfef177a09292097e9ef6ba1a95bea4a67638f3
zack junior <zacarias1992.12.19@gmail.com>
2024-05-16 09:13:32 -0300

    add URL and Token of getresponse API

--- loja_virtual-main/src/main/java/br/com/zlab/loja_virtual/util/ApiTokenIntegracao.java
+++ loja_virtual-main/src/main/java/br/com/zlab/loja_virtual/util/ApiTokenIntegracao.java
@@ -9,4 +9,8 @@ public class ApiTokenIntegracao {
 	public static final String URL_MELHOR_ENVIO_SAND_BOX = "https://sandbox.melhorenvio.com.br/";
     public static final String TOKEN_PRIVATE_JUNO = "39F561904D53B1A6B198CDF6F775D911D0FA3CDBE3C6CD69117E9BEB3BE54D1E";
     public static final String CHAVE_BOLETO_PIX = "7cf146ca-9230-4d77-ad3d-2778cafcd856";
+    public static final String TOKEN_GET_RESPONSE = "u6keyrsoo2yx792dpw13xeb1yuhm9nu6";
+    public static final String URL_ENDPOINT_GET_RESPONSE = "https://api.getresponse.com/v3/";
+
+
 }
